3042a4f9af2f67a032c2c66620687e29bea3491b
corey <coreylovette@gmail.com>
2021-03-26 18:11:19 -0500

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/dynamicserv.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/dynamicserv.dir/main.cpp.o"
+  "dynamicserv"
+  "dynamicserv.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/dynamicserv.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
